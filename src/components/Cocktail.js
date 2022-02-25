b1e747f0a25f713114c4bcb799494ed6e2b677f3
hritik <86347286+hritikd3@users.noreply.github.com>
2022-02-25 13:34:38 +0530

    fix: changes

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -8,10 +8,9 @@ export default function Cocktail({ image, name, id, info, glass }) {
       </div>
       <div className="cocktail-footer">
         <h3>{name}</h3>
-        <h4>{glass}</h4>
-        <p>{info}</p>
+        <h4>{glass}</h4>;<p>{info}</p>
         <Link to={`/cocktail/${id}`} className="btn btn-primary btn-details">
-          details
+          detail
         </Link>
       </div>
     </article>
